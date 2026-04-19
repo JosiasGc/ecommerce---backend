cedc18141ea709c17fadbe09a2871bc2398192f7
Josias Garcia <realgjosdan@gmail.com>
2026-04-19 16:35:11 -0500

    fix puerto para render

--- server.js
+++ server.js
@@ -19,8 +19,10 @@ app.get("/", (req, res) => {
   res.send("Servidor funcionando 🚀");
 });
 
-app.listen(5000, () => {
-  console.log("Servidor corriendo en puerto 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto " + PORT);
 });
 
 app.get("/api/products", (req, res) => {
